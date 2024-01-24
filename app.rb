2116d3f0c77ca7cfbe594be07e451da8b63cbd89
Daisy Chew <150085367+daisychew@users.noreply.github.com>
2024-01-24 00:40:16 +0000

    added last page of conversion

--- app.rb
+++ app.rb
@@ -1,9 +1,72 @@
+# /app.rb
+
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
+exchange_rates_key = ENV.fetch("EXCHANGE_RATES_KEY")
+
+# define a route
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  # build the API url, including the API key in the query string
+  api_url = "http://api.exchangerate.host/list?access_key=#{exchange_rates_key}"
+
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @currencies_hash = parsed_data.fetch("currencies")
+
+  # render a view template where I show the symbols
+  erb(:homepage)
+end
+
+get("/dynamic/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "http://api.exchangerate.host/list?access_key=#{exchange_rates_key}"
+  
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @currencies_hash = parsed_data.fetch("currencies")
+
+  # render a view template where I show the symbols
+  erb(:from_currency)
+end
+
+get("/dynamic/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "http://api.exchangerate.host/convert?access_key=#{exchange_rates_key}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+  # use HTTP.get to retrieve the API information
+  raw_data = HTTP.get(api_url)
+
+  # convert the raw request to a string
+  raw_data_string = raw_data.to_s
+
+  # convert the string to JSON
+  parsed_data = JSON.parse(raw_data_string)
+
+  # get the symbols from the JSON
+  @currencies_hash = parsed_data.fetch("currencies")
+
+  # render a view template where I show the symbols
+  erb(:to_currency)
 end
